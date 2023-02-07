3ea4b12557fd47e04d2fac12989ecf40edf227ed
Çağrı Ergin <121909816+cagrierginn@users.noreply.github.com>
2023-02-07 03:54:05 +0300

    TM iOS Bootcamp Odev 1

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,70 @@
+
+var sehir = "PARİS"
+print(sehir)
+var ulke = "FRANSA"
+print(ulke)
+var telefon = "+90"
+print(telefon)
+var postaKodu = "35000"
+print(postaKodu)
+var email = "fr@support.fr"
+print(email)
+var meslek = "CivilEngineer"
+print(meslek)
+var stokMiktari = "200"
+print(stokMiktari)
+var bakiye = "200₺"
+print(bakiye)
+var dogumGunu = "11.05.1993"
+print(dogumGunu)
+var maas = "1000₺"
+print(maas)
+var urunYorum = "kaliteli"
+print(urunYorum)
+
+var odemeTarihi = "01.01.2023"
+print(odemeTarihi)
+var odeme = "200₺"
+print(odeme)
+var siparisAdeti = "6000"
+print(siparisAdeti)
+var arabaModeli = "Duster"
+print(arabaModeli)
+var kitapAdi = "Sil Bastan"
+print(kitapAdi)
+var odaSayisi = "3+1"
+print(odaSayisi)
+var indirimMiktari = "%30"
+print(indirimMiktari)
+var urunAdi = "Kalemlik"
+print(urunAdi)
+var yemekFiyati = "100₺"
+print(yemekFiyati)
+var marka = "Marka"
+print(marka)
+var muzikAdi = "Muse"
+print(muzikAdi)
+var videoSuresi = "5 Dakika"
+print(videoSuresi)
+var dosyaFormati = "pdf"
+print(dosyaFormati)
+var renk = "Kırmızı"
+print(renk)
+var telefonModeli = "K50 Gaming"
+print(telefonModeli)
+var ekranBoyutu = "17.3 inch"
+print(ekranBoyutu)
+var ulusalGun = "30 Ekim"
+print(ulusalGun)
+var tatilGun = "1 Mayıs"
+print(tatilGun)
+var sokakAdi = "Mimar Sinan"
+print(sokakAdi)
+var kalanDakika = "100"
+print(kalanDakika)
+var kuponSuresi = "5 Gün"
+print(kuponSuresi)
+var kuponKodu = "abcde"
+print(kuponKodu)
+var faturaAdresi = "x mahallesi y sokak"
+print(faturaAdresi)
